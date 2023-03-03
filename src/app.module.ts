37a053a2e56508977167396d268255c15fd474c7
Maximilian Walterskirchen <mw@functn.com>
2023-03-03 17:10:09 +0100

    fix(docker): exposing correct port so container is reachable

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { TaskSchedulerService } from './task-scheduler/task-scheduler.service';
 import { CachingService } from './caching.service';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), ConfigModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: () => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, TaskSchedulerService, CachingService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
+const now = performance.now();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: [process.env.NODE_ENV === 'production' ? 'error' : 'debug'],
-  });
-  await app.listen(process.env.PORT || 8080);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const logger = app.get(Logger);
+  const config = app.get(ConfigService);
+
+  app.useLogger(logger);
+
+  await app.listen(8080);
+
+  logger.log(`App running in [${config.get('NODE_ENV')}] mode`);
+  logger.log(`Listening at: ${await app.getUrl()}`);
+  logger.debug(`App took ${Math.round(performance.now() - now)}ms to start.`);
 }
 bootstrap();
